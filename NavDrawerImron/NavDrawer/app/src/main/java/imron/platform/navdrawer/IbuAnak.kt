0ceb78ba10661acb34527a7375f17be264a03409
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2022-02-15 13:36:16 +0700

    Add files via upload

---
+++ NavDrawerImron/NavDrawer/app/src/main/java/imron/platform/navdrawer/IbuAnak.kt
@@ -0,0 +1,12 @@
+package imron.platform.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import imron.platform.navdrawer.R
+
+class IbuAnak : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ibu_anak)
+    }
+}
---
+++ NavDrawerImron/NavDrawer/app/src/main/java/imron/platform/navdrawer/Makanan.kt
@@ -0,0 +1,12 @@
+package imron.platform.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import imron.platform.navdrawer.R
+
+class Makanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_makanan)
+    }
+}
